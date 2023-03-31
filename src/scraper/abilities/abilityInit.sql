1707c862b1b62d689229c5bd1cce38daff04202c
jupiter <jupiter@students.cs.ubc.ca>
2023-03-30 20:36:18 -0700

    added insert statements for ability and has tables

---
+++ src/scraper/abilities/abilityInit.sql
@@ -0,0 +1,6 @@
+CREATE TABLE ABILITY (
+     id NUMBER(3),
+     aName CHAR(20) NOT NULL,
+     isSignature CHAR(1),
+     PRIMARY KEY (id) 
+);
---
+++ src/scraper/has/hasInit.sql
@@ -0,0 +1,6 @@
+CREATE TABLE HAS (
+     pokmonID NUMBER(4),
+     abilityID NUMBER(3),
+     isHidden CHAR(1),
+     PRIMARY KEY (pokmonID, abilityID)
+);
